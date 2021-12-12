ce321dc542820c132ea6e4a4ba8b1d19fcf8fff3
hohno-46466 <hohno.46466@gmail.com>
2021-12-13 00:42:18 +0900

    updated by @hohno_at_kuimc on penguin

--- src/sketch_20210906a_74HC595/myself.h
+++ src/sketch_20210906a_74HC595/myself.h
@@ -1,23 +1,25 @@
-#ifndef __COMMON_MYSELF_H__
-#define __COMMON_MYSELF_H__
+#ifndef __MYSELF_H__
+#define __MYSELF_H__
 
-// myself.h
+// home/hohno46466/GitHub/cZone--Arduino--ArrowLight/src/sketch_20210906a_74HC595//myself.h
 
-// First version:
+// First version: Mon Sep 6 00:00:00 JST 2021
 // Last update:
 
 // ---------------------------------------------------------
 
-#define PNAME   "Arduino/20YY/sketch_20YYMMDDa_PPPP_QQQQ"
-#define PVER    "20YY-MM-DD-A"
-#define PMAGIC  "000000"
+#define PNAME   "Arduino/2021/sketch_20210906a_74HC595"
+#define PVER    "2021-09-06-A"
+#define PMAGIC  "67feb5"
 #define PMEMO   "(none)"
 
 // Note: The PMAGIC is a magic number, which can be calculated using the following command line with "PNAME" and "PVER".
 // echo -n "$PNAME $PVER" | sha256deep | fold -6 | head -1
 
+//  Note: echo -n  "Arduino/2021/sketch_20210906a_74HC595 2021-09-06-A" | sha256sum | fold -6 | head -1
+//
 void showOpeningMessage();
 
 // ---------------------------------------------------------
 
-#endif // __COMMON_MYSELF_H__
+#endif // __MYSELF_H__
